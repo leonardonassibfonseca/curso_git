e92e9dd97cbac3c2f196475d7c05d1c275918de8
Leonardo Fonseca <fonseca3@gmail.com>
2023-09-25 19:18:03 -0300

    asasasasasas

--- src/answers.py
+++ src/answers.py
@@ -3,7 +3,6 @@ from io import BytesIO
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import XlsxWriter
 
 def rd1_question_9(df):
     df_grouped = df[["id", "seller_type"]].groupby("seller_type")
